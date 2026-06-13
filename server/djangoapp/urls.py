7a024bb33fc62d4cb3e423e13147eee114037ab6
Devin <DevinChaseDaley@gmail.com>
2026-06-13 13:08:42 -0400

    more formatting

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path(route='add_review',
          view=views.add_review, name='add_review'),
     path(route='get_cars',
-         view=views.get_cars, name ='getcars'),
+         view=views.get_cars, name='getcars'),
     path(route='reviews/dealer/<int:dealer_id>',
          view=views.get_dealer_reviews, name='get_dealer_reviews'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
